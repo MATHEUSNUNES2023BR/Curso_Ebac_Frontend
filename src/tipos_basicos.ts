11545d770bd07eec063cf6c93fbe710eddd0adad
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-02-03 13:40:07 -0300

    Exercício finalizado

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,10 @@
+// Função multiplicadora
+function multiplicadora(a:number, b:number):number{
+    return a * b
+}
+
+// Função Saudação
+const saudacao =  (nome:string): string => `Olá ${nome}`
+
+console.log(multiplicadora(2, 4))
+console.log(saudacao('matheus'))
