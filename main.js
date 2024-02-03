04c042625de3e109d49bfb87d97a4e87d035d7e6
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-02-03 07:11:22 -0300

    Finalizado

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const alunos = [
     {'nome': 'Matheus', 'nota': 10}, {'nome': 'João', 'nota': 8}, {'nome': 'Maria', 'nota': 9},
     {'nome': 'Pedro', 'nota': 6}, {'nome': 'Debora', 'nota':4}, {'nome': 'Sofia', 'nota': 3},
-    {'nome': 'Mariana', 'nota': 1}, {'nome': 'Estefani', 'nota': 2}, {'nome': 'Ludmila', 'nota': 5}
+    {'nome': 'Mariana', 'nota': 1}, {'nome': 'Estefani', 'nota': 2}, {'nome': 'Ludmila', 'nota': 5},
+    {'nome': 'Sabrina', 'nota': 7}, {'nome': 'Jessica', 'nota': 4}, {'nome': 'Geovana', 'nota': 6}
 ]
 const alunosSelecionados = alunos.filter((e)=>{
     if(e.nota >= 6){
