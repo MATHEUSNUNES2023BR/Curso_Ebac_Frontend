e7d60d978a5258ff277da6734b4755943721f0b9
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-02-03 07:08:41 -0300

    Meus commits estão com algum erro

---
+++ main.js
@@ -0,0 +1,12 @@
+const alunos = [
+    {'nome': 'Matheus', 'nota': 10}, {'nome': 'João', 'nota': 8}, {'nome': 'Maria', 'nota': 9},
+    {'nome': 'Pedro', 'nota': 6}, {'nome': 'Debora', 'nota':4}, {'nome': 'Sofia', 'nota': 3},
+    {'nome': 'Mariana', 'nota': 1}, {'nome': 'Estefani', 'nota': 2}, {'nome': 'Ludmila', 'nota': 5}
+]
+const alunosSelecionados = alunos.filter((e)=>{
+    if(e.nota >= 6){
+        return e
+    }
+})
+
+console.log(alunosSelecionados)
